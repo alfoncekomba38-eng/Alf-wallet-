8f81ac5cb81683357e0f93fcf9bc214c1a8d0ca2
alfoncekomba38-eng <alfoncekomba38@gmail.com>
2026-04-28 13:06:17 +0300

    Refactor Firebase imports and initialization

--- firebase.js
+++ firebase.js
@@ -1,36 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth, signInAnonymously, onAuthStateChanged } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getAnalytics } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyCBycKh2s1Z0CDdPaMhaYUcryc0jMuCo2U",
-  authDomain: "alf-wallet.firebaseapp.com",
-  projectId: "alf-wallet",
-  storageBucket: "alf-wallet.firebasestorage.app",
-  messagingSenderId: "451766153657",
-  appId: "1:451766153657:web:6276c12f129f3b890f0668",
-  measurementId: "G-H4GKSK1CZT"
+  // your config hapa
 };
 
-// INIT
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
 
-// AUTO LOGIN
+// AUTH
+const auth = getAuth(app);
+
+// AUTO LOGIN (IMPORTANT)
 signInAnonymously(auth)
-  .catch(err => console.error("Login error:", err));
+  .then(() => {
+    console.log("User signed in anonymously ✔");
+  })
+  .catch((error) => {
+    console.error("Auth error:", error);
+  });
 
-// AUTH LISTENER
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("USER ACTIVE:", user.uid);
-  } else {
-    console.log("NO USER");
-  }
-});
+// FIRESTORE
+const db = getFirestore(app);
+
+export { auth, db };
