1407d158c4f79bae0f1b11982c267afeee3a7306
alfoncekomba38-eng <alfoncekomba38@gmail.com>
2026-04-28 06:55:33 +0300

    Modify Firebase config and add authentication
    Updated Firebase configuration with placeholders for keys and added authentication setup.

--- firebase.js
+++ firebase.js
@@ -1,15 +1,22 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {
+  getFirestore, doc, getDoc, setDoc, updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {
+  getAuth, signInAnonymously
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBycKh2s1Z0CDdPaMhaYUcryc0jMuCo2U",
-  authDomain: "alf-wallet.firebaseapp.com",
-  projectId: "alf-wallet",
-  storageBucket: "alf-wallet.firebasestorage.app",
-  messagingSenderId: "451766153657",
-  appId: "1:451766153657:web:6276c12f129f3b890f0668"
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+await signInAnonymously(auth);
