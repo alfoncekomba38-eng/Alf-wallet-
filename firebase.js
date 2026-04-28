8d704e6515e479a4237044153755f04e993a3613
alfoncekomba38-eng <alfoncekomba38@gmail.com>
2026-04-29 00:08:34 +0300

    Refactor Firebase imports and streamline login handling
    Updated Firebase imports to use CDN links and simplified login success logging.

--- firebase.js
+++ firebase.js
@@ -1,45 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// 🔥 YOUR REAL CONFIG (from Firebase)
 const firebaseConfig = {
   apiKey: "AIzaSyCBycKh2s1Z0CDdPaMhaYUcryc0jMuCo2U",
   authDomain: "alf-wallet.firebaseapp.com",
-  databaseURL: "https://alf-wallet-default-rtdb.firebaseio.com",
   projectId: "alf-wallet",
   storageBucket: "alf-wallet.firebasestorage.app",
   messagingSenderId: "451766153657",
-  appId: "1:451766153657:web:6276c12f129f3b890f0668",
-  measurementId: "G-H4GKSK1CZT"
+  appId: "1:451766153657:web:6276c12f129f3b890f0668"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Auth
 const auth = getAuth(app);
-
-// Firestore
 const db = getFirestore(app);
 
-// 🔥 AUTO LOGIN
 signInAnonymously(auth)
-  .then(() => {
-    console.log("Anonymous login success ✔");
-  })
-  .catch((error) => {
-    console.error("Auth error:", error);
-  });
-
-// 🔥 USER STATE LISTENER
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("User UID:", user.uid);
-    localStorage.setItem("alf_uid", user.uid);
-  } else {
-    console.log("No user logged in");
-  }
-});
+  .then(() => console.log("Anonymous login success"))
+  .catch((error) => console.error(error));
 
 export { auth, db };
